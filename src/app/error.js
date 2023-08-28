30a98941f7df7eb56bd552b5fb81dbc46d7d48f9
Max Hargreave <max@monsun.dk>
2023-08-28 16:13:58 +0200

    error.js

--- src/app/error.js
+++ src/app/error.js
@@ -3,22 +3,20 @@
 import { useEffect } from 'react'
  
 export default function Error({ error, reset }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+    
+    return (
+        <main className="min-h-[calc(100vh-72px)] flex flex-col">
+            <section className="flex-1 dashboard-section flex items-center">
+                <div className="container max-w-6xl mx-auto py-12 px-6 text-center mb-16">
+                    <h1 className='text-6xl uppercase font-bold tracking-wider mb-6'>Don't Panic!</h1>
+                    <p className='text-2xl mb-8'>Something went wrong but we are working on it.</p>
+                    <button onClick={() => reset()} className="bg-blue-900 hover:bg-blue-700 text-white text-sm py-2 px-4 rounded mt-4">Try again</button>
+                </div>
+            </section>
+        </main>
+    )
 }
